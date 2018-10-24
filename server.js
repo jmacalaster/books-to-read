87e1af63b2277a49136914548db049ca29f825db
JMac <julia@defmethod.com>
2018-10-23 21:47:06 -0400

    Mongoose Connection

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/books-to-read";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 mongoose.Promise = Promise;
 
 // Routes
